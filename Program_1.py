1bc9ab30badb5afc2f5251da40121da9e547eb95
ismail123ngk <99345471+ismail123ngk@users.noreply.github.com>
2023-06-21 21:46:49 +0530

    Update Program_1.py

--- Program_1.py
+++ Program_1.py
@@ -3,13 +3,11 @@ from flask import Flask
 app = Flask('app')
 
 
-@app.route('/home')
-def hello_world():
-  return 'Hello, World! Ismail'
+@app.route('/index')
+def index():
+  return 'This is index'
 
 
 if __name__ == '__main__':
   app.run(debug=True)
-#making a small change
-
 #We are done with this version and we changed the file to python file
